71d39b6d05451846debb5c9214d77963e698d463
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2013-04-03 19:01:01 +0000

    commit

--- trunk/drivers/ata/ata.c
+++ trunk/drivers/ata/ata.c
@@ -1,21 +1,21 @@
+#include "asm.h" 
 #include "drivers/ata/ata.h" 
 
 void init_ata()
 {	
 	struct t_i_desc i_desc;
 	
-	//printk("kbc init \n");
 	in_buf=new_queue();
-	i_desc.baseLow=((int)&int_handler_kbc) & 0xFFFF;
+	i_desc.baseLow=((int)&int_handler_ata) & 0xFFFF;
 	i_desc.selector=0x8;
-	i_desc.flags=0x0EF00;//0x08e00;
+	i_desc.flags=0x0EF00;
 	i_desc.baseHi=((int)&int_handler_kbc)>>0x10;
-	set_idt_entry(0x21,&i_desc);
+	set_idt_entry(0x2E,&i_desc);
 }
 
 void free_ata()
 {
-	kfree(in_buf);
+
 }
 
 void int_handler_ata()
@@ -23,8 +23,16 @@ void int_handler_ata()
 
 }
 
-void read_28_ata()
+void read_28_ata(unsigned int lba)
 {
+//Send 0xE0 for the "master" or 0xF0 for the "slave", ORed with the highest 4 bits of the LBA to port 0x1F6: outb(0x1F6, 0xE0 | (slavebit << 4) | ((LBA >> 24) & 0x0F))
+//Send a NULL byte to port 0x1F1, if you like (it is ignored and wastes lots of CPU time): outb(0x1F1, 0x00)
+//Send the sectorcount to port 0x1F2: outb(0x1F2, (unsigned char) count)
+//Send the low 8 bits of the LBA to port 0x1F3: outb(0x1F3, (unsigned char) LBA))
+//Send the next 8 bits of the LBA to port 0x1F4: outb(0x1F4, (unsigned char)(LBA >> 8))
+//Send the next 8 bits of the LBA to port 0x1F5: outb(0x1F5, (unsigned char)(LBA >> 16))
+//Send the "READ SECTORS" command (0x20) to port 0x1F7: outb(0x1F7, 0x20) 
+	out(0x1F6, 0xE0 | (LBA >> 24))
 
 }
 
