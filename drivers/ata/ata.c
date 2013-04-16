527de19b618e849562150564c22a9b832165a7ca
giumaug <giumaug@gmail.com>
2013-04-16 12:03:51 +0000

    commit

--- drivers/ata/ata.c
+++ drivers/ata/ata.c
@@ -66,7 +66,10 @@ void _read_28_ata(t_ata_request *ata_request,struct t_processor_reg* processor_r
 
 void _write_28_ata(t_ata_request *ata_request,struct t_processor_reg* processor_reg)
 {
-	int i=0;	
+	int i=0;
+	int z=0;
+	int kk;
+	void *io_buffer;	
 	SAVE_IF_STATUS
 	CLI
 	//sleep process if dirver locked by other request
@@ -78,22 +81,29 @@ void _write_28_ata(t_ata_request *ata_request,struct t_processor_reg* processor_
 	out((unsigned char)(ata_request->lba >> 8),0x1F4);
 	out((unsigned char)(ata_request->lba >> 16),0x1F5);
 	out(WRITE_28,0x1F7);
-	for(i=0;i<=10000;i++);
+	for(i=0;i<=1000000;i++);
+	kk=in(0x1F7);
 	//(inb(iobase + ATA_STATUS) & ATA_STATUS_ERR) 
-	if (in(0x1F7) & 1)
+	//if (in(0x1F7) & 1)
 	{
-		i++;
+		z++;
 	}
-	asm("push %ecx");
-	asm("push %edx");
-	asm("push %edi");
-	asm("mov $0x80,%ecx");
-	asm("mov $0x1F0,%edx");
-	asm ("movl %0,%%edi;"::"r"(current_ata_request->io_buffer));                                  
-	asm("rep outsw");
-	asm("pop %edi");
-	asm("pop %edx");
-	asm("pop %ecx");
+	io_buffer=current_ata_request->io_buffer;
+//	asm("push %ecx");
+//	asm("push %edx");
+//	asm("push %edi");
+//	asm("mov $0x80,%ecx");
+//	asm("mov $0x1F0,%edx");
+//	asm ("movl %0,%%edi;"::"r"(current_ata_request->io_buffer));                                  
+//	asm("rep outsw");
+//	asm("pop %edi");
+//	asm("pop %edx");
+//	asm("pop %ecx");
+
+	for (i =0;i<512;i++)
+	{  
+		out(*(char*)io_buffer++,0x1F0);  
+	}
 	//_sleep(processor_reg);
 	RESTORE_IF_STATUS
 }
