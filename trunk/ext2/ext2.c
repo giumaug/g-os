434cd7c6d9869ff0adc020dfbf7c85876fbb661d
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2013-07-12 14:12:07 +0000

    commit

--- trunk/ext2/ext2.c
+++ trunk/ext2/ext2.c
@@ -5,8 +5,6 @@
 
 int open(const char *path, int flags, mode_t mode); 
 {
-	//se file esiste carico inode da disco 
-	//se file non esiste alloco inode
 	u32 fd;
 	struct t_process_context* current_process_context;
 	t_inode* inode;
@@ -40,7 +38,6 @@ int close(int fd)
 	kfree(inode);
 }
 
-//return byte read
 int read(int fd, void *buf, size_t count); 
 {
 	u32 i;	
@@ -48,50 +45,133 @@ int read(int fd, void *buf, size_t count);
 	u32 first_data_offset;
 	u32 last_inode_block;
 	u32 last_data_offset;
-
+	u32 inode_block;
 	u32 lba;
+	u32 indirect_lba;
+	u32 allocated_indirect_block;
 	u32 sector_count;
+	u32 byte_to_read;
+	u32 byte_read;
+	u32 byte_count;
 	t_inode* inode;
 	void io_buffer;
+	void io_buffer_2;
 
+	byte_read=0;
 	io_buffer=kmalloc(BLOCK_SIZE);
+	io_buffer_2=kmalloc(BLOCK_SIZE);
 	inode=hashtable_get(current_process_context->file_desc,fd);
 	first_inode_block=inode->file_offset/BLOCK_SIZE;
 	first_data_offset=inode->file_offset%BLOCK_SIZE;
 	last_inode_block=(inode->file_offset+count)/BLOCK_SIZE;
 	last_data_offset=(inode->file_offset+count)%BLOCK_SIZE;
 
+	allocated_indirect_block=0;
 	for (i=first_inode_block;i<=first_inode_block;i++)
 	{
-		if (i>11)
+		if (i>12)
 		{
-			lba=inode->i_block[first_inode_block];
-        		sector_count=BLOCK_SIZE/SECTOR_SIZE;
-			_read_28_ata(sector_count,lba,io_buffer,TRUE);
-			inode_block=io_buffer[inode_block-11-1];	
+			if (!allocated_indirect_block)
+			{
+				indirect_lba=inode->i_block[12];
+        			sector_count=BLOCK_SIZE/SECTOR_SIZE;
+				_read_28_ata(sector_count,indirect_lba,io_buffer,TRUE);
+				allocated_indirect_block=1;
+			}
+			lba=io_buffer[inode_block-12];	
 		}
 		else
 		{
-			inode_block=i;
+			lba=i;
 		}
-		lba=inode->i_block[first_inode_block];
+		if(i==first_inode_block)
+		{
+			----------------------qui
+		}
+		else if (byte_to_read>=(BLOCK_SIZE/SECTOR_SIZE))
+		{
+			byte_count=BLOCK_SIZE/SECTOR_SIZE;
+			byte_to_read-=BLOCK_SIZE/SECTOR_SIZE;	
+		}
+		else
+		{
+			byte_count=byte_to_read;
+		}
         	sector_count=BLOCK_SIZE/SECTOR_SIZE;
-		_read_28_ata(sector_count,lba,io_buffer,TRUE);
-		------------qui		
-		
+		_read_28_ata(sector_count,lba,io_buffer_2,TRUE);
+		kmemcpy(buf,io_buffer_2,byte_count);
+		inode->file_offset+=byte_to_read;
+		buf+=byte_to_read;
+		byte_read+=byte_count;
 	}
-
-	
-	
-
-
 	kfree(io_buffer);
+	return byte_read;
 }
 
-//return write byte
  int write(int fd, const void *buf, size_t count)
 {
+	{
+	u32 i;	
+	u32 first_inode_block;
+	u32 first_data_offset;
+	u32 last_inode_block;
+	u32 last_data_offset;
+	u32 inode_block;
+	u32 lba;
+	u32 indirect_lba;
+	u32 indirect_old_lba;
+	u32 sector_count;
+	u32 byte_to_read;
+	u32 byte_read;
+	u32 byte_count;
+	t_inode* inode;
+	void io_buffer;
+	void io_buffer_2;
 
+	byte_written=0;
+	io_buffer=kmalloc(BLOCK_SIZE);
+	io_buffer_2=kmalloc(BLOCK_SIZE);
+	inode=hashtable_get(current_process_context->file_desc,fd);
+	first_inode_block=inode->file_offset/BLOCK_SIZE;
+	first_data_offset=inode->file_offset%BLOCK_SIZE;
+	last_inode_block=(inode->file_offset+count)/BLOCK_SIZE;
+	last_data_offset=(inode->file_offset+count)%BLOCK_SIZE;
+
+	byte_to_write=count;
+	for (i=first_inode_block;i<=first_inode_block;i++)
+	{
+		if (byte_to_write>=(BLOCK_SIZE/SECTOR_SIZE))
+		{
+			byte_count=BLOCK_SIZE/SECTOR_SIZE;
+			byte_to_write-=BLOCK_SIZE/SECTOR_SIZE;	
+		}
+		else
+		{
+			byte_count=byte_to_write;
+		}
+		lba=alloc_block(ext2,inode,i);
+		if (i>12)
+		{
+			if (inode->i_block[12]==0)
+			{
+				inode->i_block[11]=alloc_indirect_block(ext2,inode);	
+			}
+			io_buffer[i-12]=lba;
+		}
+		else 
+		{
+			inode->i_block[i]=lba;
+		}
+		kmemcpy(io_buffer_2,buf,byte_count);
+		sector_count=BLOCK_SIZE/SECTOR_SIZE;
+		_write_28_ata(sector_count,lba,io_buffer_2,TRUE);
+		inode->file_offset+=byte_count;
+		buf+=byte_count;
+		byte_written+=byte_count;
+	}
+	kfree(io_buffer);
+	kfree(io_buffer_2);
+	return byte_written;
 }
 
 void rm()
