c4f6fa2772f68ffd41dc5a81baa4614cf2df1783
giumaug <giumaug@gmail.com>
2013-07-18 16:23:17 +0000

    commit

--- klib/kmemcpy.c
+++ klib/kmemcpy.c
@@ -4,8 +4,21 @@ void *kmemcpy(void *dest, const void *src, unsigned int size)
 {
 	char *dest_addr = dest;
 	while (size-- > 0)
+	{
     		*(char*)dest++ = *(char*)src++;
+	}
   	return dest_addr;
 }
+
+void kfillmem(void *dest, const int val, unsigned int size)
+{
+	char *dest_addr = dest;
+	while (size-- > 0)
+	{
+    		*(char*)dest++ = val;
+	}
+  	return dest_addr;
+}
+
 
 
