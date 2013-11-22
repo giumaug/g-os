8595b99e35398d078f0639621488badcfaa8f4c9
giumaug <giumaug@gmail.com>
2013-11-22 17:20:03 +0000

    commit

--- process/process_2.c
+++ process/process_2.c
@@ -11,18 +11,25 @@ process_2()
 	unsigned int workload;
 	unsigned int seed=SEED;
 
-	workload=(_rand(&seed) % 20 + 1);
-
-	for (y=0;y<workload;y++)
-	{
-		mem_addr=malloc(512);
-		for (z=0;z<512;z++)
-		{
-			*(mem_addr+z)=0xFF;
-		}
-		free(mem_addr);
-	}
+//	workload=(_rand(&seed) % 20 + 1);
+//
+//	for (y=0;y<workload;y++)
+//	{
+//
+//		mem_addr=malloc(512);
+//		for (z=0;z<512;z++)
+//		{
+//			*(mem_addr+z)=0xFF;
+//		}
+//		_write_28_ata(system->device_desc,1,10,mem_addr);
+//
+//		free(mem_addr);
+//	}
+	write_mem_addr=malloc(512);
+	read_mem_addr=malloc(512);
 	_write_28_ata(system->device_desc,1,10,mem_addr);
-	_read_28_ata(t_device_desc* device_desc,unsigned int sector_count,unsigned int lba,void* io_buffer)
+	_read_28_ata(system->device_desc,1,10,mem_addr);
+	free(write_mem_addr);
+	free(read_mem_addr);
 	exit(0);
 }
