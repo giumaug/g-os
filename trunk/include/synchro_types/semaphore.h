8510c30fcde71efc0a9099f7b027e0ac3b85f8a1
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2014-01-12 19:20:21 +0000

    commit

--- trunk/include/synchro_types/semaphore.h
+++ trunk/include/synchro_types/semaphore.h
@@ -7,13 +7,13 @@
 
 typedef struct s_sem_desc 
 {
-	t_llist* wait_queue wait_queue;
+	t_llist* wait_queue;
 	unsigned int count;
 	unsigned int spinlock;
 }
 t_sem_desc;
 
-void sem_int(t_sem_desc sem_desc);
+void sem_init(t_sem_desc sem_desc);
 void sem_down(t_sem_desc sem_desc);
 void sem_up(t_sem_desc sem_desc);
 
