37c4f20dc5d036476afdcb9b8866359b843f6adb
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2014-08-08 12:28:18 +0000

    commit

--- trunk/process/process_1.c
+++ trunk/process/process_1.c
@@ -1,45 +1,4 @@
-#include "lib/stdio.h"
-#include "lib/unistd.h"
-#include "lib/fcntl.h"
-#include "lib/stdlib.h"
-
-/*
-process_1()
-{
-	unsigned int n_request;
-	unsigned int i;
-	unsigned int age=0;
-	unsigned int pid;
-	unsigned int params[1];
-	unsigned int count;
-	unsigned int ret;
-	
-	while(1)
-	{
-		n_request=(rand() % 100 + 1);
-//		n_request=25;
-		n_request=10;
-		printf("\nage=");
-		d_printf(age);
-		printf("\nrequest number.....=");
-		d_printf(n_request);
-		for (i=0;i<n_request;i++)
-		{	
-			pid=fork();
-			if (pid==0)
-			{	
-				exec(0x700000,0x100000);
-			}
-		}
-		age++;
-		printf("\n+++++++++");
-		sleep(60000);
-		printf("\n.........");
-		check_free_mem();
-	}
-	exit(0);
-}
-*/
+#include "lib/lib.h"
 
 process_1()
 {
