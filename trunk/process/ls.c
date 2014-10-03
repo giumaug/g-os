e5b7348f1ce9296f893f4cab6253e1bdcc692ad0
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2014-10-03 14:45:13 +0000

    commit

--- trunk/process/ls.c
+++ trunk/process/ls.c
@@ -1,17 +1,28 @@
-#include "lib/lib.h"  
+#include "lib/lib.h"
+
+#define DIR_SIZE 1024
+#define READ_BYTE(src,dst) do {unsigned short* p;p=src;dst=(*p)&0xff;} while(0);
+#define READ_WORD(src,dst) do {unsigned short* p;p=src;dst=*p;} while(0);
+#define READ_DWORD(src,dst) do {unsigned int* p;p=src;dst=*p;} while(0);
 
 int main(int argc, char *argv[])
 {
+	int f;
+	char* io_buffer;
+	char file_name[100];
+	unsigned int next_entry;
+	unsigned int name_len;
+	unsigned int rec_len;
+
 	printf("\n");
 	printf("argc=");
 	print_num(argc);
 	printf("\n");
-	printf("argv[0]=");
-	printf(argv[0]);
+	printf("argv[1]=");
+	printf(argv[1]);
 	printf("\n");
 
-	read(f,io_buffer,18736);
-	close(f);
+	io_buffer=malloc(DIR_SIZE);
 	
 	if (argc>1)
 	{
@@ -22,9 +33,23 @@ int main(int argc, char *argv[])
 		//nedd to propagate from parent to child look current_dir_inode_number inside lookup_inode
 		f=open("./", O_RDWR | O_APPEND);
 	}
+	read(f,io_buffer,DIR_SIZE);
+	close(f);
 
-
-
-
+	next_entry=0;
+	j=0;
+	while(next_entry<=DIR_SIZE)
+	{
+		READ_BYTE(&io_buffer[next_entry+6],name_len);
+		READ_WORD(&io_buffer[next_entry+4],rec_len);
+		while(j<name_len)		
+		{
+			file_name[j]=io_buffer[next_entry+8+j];
+			j++;
+		}
+		next_entry+=rec_len;
+		printf(file_name);
+	}
+	free(DIR_SIZE);
 	exit(0);
 }
