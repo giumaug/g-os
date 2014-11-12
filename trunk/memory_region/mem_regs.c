8404ad1dcf970d18f89f7d068a609a448b7bac43
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2014-11-12 13:02:25 +0000

    commit

---
+++ trunk/memory_region/mem_regs.c
@@ -0,0 +1,30 @@
+
+
+typedef struct s_mem_regs 
+{ 
+	u32 start_addr;
+	u32 end_addr;
+	
+} 
+t_mem_regs;
+
+void create_mem_reg()
+{
+	t_llist *new_dllist();
+
+}
+
+void delete_mem_reg()
+{
+
+}
+
+void enlarge_mem_reg()
+{
+
+}
+
+void shrink_mem_reg()
+{
+
+}
