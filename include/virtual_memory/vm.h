07beb8d5fcf9267bb66b5f62a5f05d9422a284dc
giumaug <giumaug@gmail.com>
2014-11-21 13:30:07 +0000

    commit

--- include/virtual_memory/vm.h
+++ include/virtual_memory/vm.h
@@ -34,6 +34,7 @@ void* init_vm_process(struct t_process_context* process_context);
 void free_vm_process(struct t_process_context* process_context);
 void map_vm_mem(void* page_dir,unsigned int vir_mem_addr,unsigned int phy_mem_addr,int mem_size);
 void umap_vm_mem(void* page_dir,unsigned int virt_mem_addr,unsigned int mem_size,unsigned int flush);
+void clone_vm_process(void* parent_page_dir);
 
 #endif
 
