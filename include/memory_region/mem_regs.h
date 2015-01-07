babef4a2890805c622a402cff12d256e8eac6957
giumaug <giumaug@gmail.com>
2015-01-07 12:36:59 +0000

    commit

---
+++ include/memory_region/mem_regs.h
@@ -0,0 +1,16 @@
+#ifndef MEM_REGS_H                
+#define MEM_REGS_H
+
+typedef struct s_mem_reg
+{ 
+	u32 start_addr;
+	u32 end_addr;
+	
+} 
+t_mem_reg;
+
+t_mem_reg* create_mem_reg(u32 start_addr,u32 end_addr);
+void delete_mem_reg(t_mem_reg* mem_reg);
+
+#endif
+
