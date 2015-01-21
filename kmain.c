4c416ba58bfe31c330285c5f67722b457011590a
giumaug <giumaug@gmail.com>
2015-01-21 14:01:18 +0000

    commit

--- kmain.c
+++ kmain.c
@@ -98,7 +98,7 @@ void kmain( void* mbd, unsigned int magic,int init_data_add)
 	*(system.process_info->tss.ss)=0x18;
 	*(system.process_info->tss.esp)=KERNEL_STACK;
 
-	map_vm_mem(process_context->page_dir,(KERNEL_STACK-KERNEL_STACK_SIZE),process_context->phy_kernel_stack,KERNEL_STACK_SIZE);
+	//map_vm_mem(process_context->page_dir,(KERNEL_STACK-KERNEL_STACK_SIZE),process_context->phy_kernel_stack,KERNEL_STACK_SIZE);
 	SWITCH_PAGE_DIR(FROM_VIRT_TO_PHY(((unsigned int) process_context->page_dir)))                           	
 	
 	kernel_stack=KERNEL_STACK;
