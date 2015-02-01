79c91429f5e812037c097e7015f1b45efaa373d4
giumaug <giumaug@gmail.com>
2015-02-01 19:05:26 +0000

    commit

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -509,14 +509,14 @@ u32 _exec(char* path,char* argv[])
 
 	char* proc_mem;
 	proc_mem=buddy_alloc_page(system.buddy_desc,0x20000);
-//	map_vm_mem(current_process_context->page_dir,PROC_VIRT_MEM_START_ADDR,FROM_VIRT_TO_PHY(proc_mem),0x20000);
+	map_vm_mem(current_process_context->page_dir,PROC_VIRT_MEM_START_ADDR,FROM_VIRT_TO_PHY(proc_mem),0x20000);
   
 
 	asm("mov $0x23,%ax;mov %ax,%ds;mov %ax,%es;push %eax;movl $0x23,%eax;push %eax;");							
         asm("movl %0,%%eax;push %%eax;"::"r"(stack_pointer)); 	
 	asm("movl $0x206,%eax;push %eax;movl $0x13,%eax;push %eax;movl $0x100000,%eax;push %eax;");
-	asm ("jmp 0x100000");                              		
-//	asm("iret;");
+//	asm ("jmp 0x100000");                              		
+	asm("iret;");
 
 //	SWITCH_TO_USER_MODE(stack_pointer)
 	return 0;
