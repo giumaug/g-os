1480d39befa21eaa5eecfe37a4ffa8a00f47c41e
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2015-02-12 13:31:40 +0000

    commit

--- trunk/virtual_memory/vm.c
+++ trunk/virtual_memory/vm.c
@@ -396,4 +396,44 @@ void page_fault_handler()
 	}	
 	ENABLE_PREEMPTION
 	EXIT_INT_HANDLER(on_exit_action,processor_reg)
+
+
+	#define EXIT_INT_HANDLER(action,processor_reg)                                                                  		\
+                                                                                                                   		\
+	static struct t_process_context _current_process_context;                                                  		\
+	static struct t_process_context _old_process_context;                                                      		\
+	static struct t_process_context _new_process_context;	                                                   		\
+	static struct t_processor_reg _processor_reg;                                                              		\
+	static unsigned int _action2;                                                                              		\
+                                                                                                                   		\
+	CLI                                                                                                        		\
+	_action2=action;                                                                                           		\
+	_current_process_context=*(struct t_process_context*)system.process_info->current_process->val;             		\
+	_old_process_context=_current_process_context;                                                             		\
+	_processor_reg=processor_reg;                                                                              		\
+	if (_action2>0)                                                                                            		\
+	{                                                                                                          		\
+		schedule(&_current_process_context,&_processor_reg);                                               		\
+		_new_process_context=*(struct t_process_context*)system.process_info->current_process->val;         		\
+		_processor_reg=_new_process_context.processor_reg;                                                              \
+		SWITCH_PAGE_DIR(FROM_VIRT_TO_PHY(((unsigned int) _new_process_context.page_dir)))                  		\
+		DO_STACK_FRAME(_processor_reg.esp-8);                                                              		\
+		if (_action2==2)                                                                                   		\
+		{                                                                                                  		\
+			DO_STACK_FRAME(_processor_reg.esp-8);                                                      		\
+			free_vm_process(&_old_process_context.page_dir);                                                 	\
+			buddy_free_page(system.buddy_desc,FROM_PHY_TO_VIRT(_old_process_context.phy_kernel_stack));             \
+			  													\
+		}                                                                                                  		\
+		RESTORE_PROCESSOR_REG                                                                              		\
+		EXIT_SYSCALL_HANDLER                                                                               		\
+	}                                                                                                          		\
+	else                                                                                                       		\
+	{                                                                                                            		\
+		RESTORE_PROCESSOR_REG                                                                              		\
+		RET_FROM_INT_HANDLER                                                                               		\
+	}
+
+
+
 }
