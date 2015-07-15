c717851ca62e225c217f9c3570359a67ff6db826
giumaug@gmail.com <giumaug@gmail.com@1f0ad3dc-7fbe-b56a-ba91-dfddcb376397>
2015-07-15 12:19:53 +0000

    commit

--- trunk/process/shell.c
+++ trunk/process/shell.c
@@ -25,7 +25,14 @@ int main (int _argc, char* _argv[])
 		printf("=>");
 		scanf("%s",&cmd);
 
-		if (cmd[0]!='/' && !(cmd[0]=='.' && cmd[1]=='/'))
+		//BUILT IN COMMAND!!!
+		if (cmd[0]=='c' && cmd[1]=='d' && cmd[2]==' ')
+		{
+				cd(&cmd[index+3]);
+				printf("\n");
+		}
+
+		else if (cmd[0]!='/' && !(cmd[0]=='.' && cmd[1]=='/'))
 		{
 			printf("\n");
 			printf("Command not found.");
@@ -41,92 +48,83 @@ int main (int _argc, char* _argv[])
 			{
 				index=2;
 			}
-		
-			//BUILT IN COMMAND!!!		
-			if (cmd[index]=='c' && cmd[index+1]=='d' && cmd[index+2]==' ')
+			
+			if (cmd[len-1]=='-') 
 			{
-				cd(&cmd[index+3]);
-				printf("\n");
+				is_background=1;
+				cmd[len-1]='\0';
 			}
-			else
-			{
-				if (cmd[len-1]=='-') 
-				{
-					is_background=1;
-					cmd[len-1]='\0';
-				}
 
-				i=0;
-				while (cmd[i]!=NULL)
+			i=0;
+			while (cmd[i]!=NULL)
+			{
+				if(cmd[i]==' ')
 				{
-					if(cmd[i]==' ')
-					{
-						argc++;
-					}
+					argc++;
+				}
+				i++;
+			}
+		
+			argv=malloc(sizeof(char*)*(argc+2));
+				
+			i=0;
+			for(k=0;k<=argc;k++)
+			{
+				printf("\n");
+				j=0;
+				while (cmd[i]!=' ' && cmd[i]!=NULL)
+				{		
+					j++;
 					i++;
 				}
-		
-				argv=malloc(sizeof(char*)*(argc+2));
-				
-				i=0;
+				i++;
+				argv[k]=malloc(j+1);
+			}
+
+			i=0;
+			j=0;
+			for(k=0;k<=argc;k++)
+			{
+				j=0;
+				while (cmd[i]!=' ' && cmd[i]!=NULL)
+				{	
+					argv[k][j]=cmd[i];
+					j++;
+					i++;
+				}
+				i++;
+				argv[k][j++]='\0';
+				printf("argv=%s \n",argv[k]);
+			
+			}
+			argv[argc+1]=NULL;
+			pid=fork();
+
+			if (pid==0)
+			{
+				//ret=execv(argv[0],argv);
+				ret=exec(argv[0],argv);
+
 				for(k=0;k<=argc;k++)
+				{
+					free(argv[k]);
+				}
+				free(argv);
+
+				if (ret==-1)
 				{
 					printf("\n");
-					j=0;
-					while (cmd[i]!=' ' && cmd[i]!=NULL)
-					{		
-						j++;
-						i++;
-					}
-					i++;
-					argv[k]=malloc(j+1);
+					printf("Command not found.");
+					printf("\n");
+					exit(0);
 				}
-
-				i=0;
-				j=0;
-				for(k=0;k<=argc;k++)
+			}
+			else 
+			{
+				if (!is_background)
 				{
-					j=0;
-					while (cmd[i]!=' ' && cmd[i]!=NULL)
-					{	
-						argv[k][j]=cmd[i];
-						j++;
-						i++;
-					}
-					i++;
-					argv[k][j++]='\0';
-					printf("argv=%s \n",argv[k]);
-			
-				}
-				argv[argc+1]=NULL;
-				pid=fork();
-
-				if (pid==0)
-				{
-					//ret=execv(argv[0],argv);
-					ret=exec(argv[0],argv);
-
-					for(k=0;k<=argc;k++)
-					{
-						free(argv[k]);
-					}
-					free(argv);
-
-					if (ret==-1)
-					{
-						printf("\n");
-						printf("Command not found.");
-						printf("\n");
-						exit(0);
-					}
-				}
-				else 
-				{
-					if (!is_background)
-					{
-						pause();
-						printf("woken up \n");
-					}
+					pause();
+					printf("woken up \n");
 				}
 			}
 		}
