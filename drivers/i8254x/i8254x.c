a224f3d0366cbd25be92a69de66a6a63eab3317e
peppe <peppe@localhost.localdomain>
2015-10-04 17:30:13 +0200

    commit

--- drivers/i8254x/i8254x.c
+++ drivers/i8254x/i8254x.c
@@ -1,3 +1,124 @@
+
+static void write_i8254x(t_i8254x* i8254x,u32 address,u32 value)
+{
+	outdw(value,i8254x->mem_base+address);
+}
+
+static u32 read_i8254x(t_i8254x* i8254x,u32 adrress)
+{
+	return indw(i8254x,CTRL_REG);
+}
+
+static u16 read_eeprom_i8254x(t_i8254x* i8254x,u8 addr)
+{
+	u32 tmp;
+
+	write_i8254x(i8254x,REG_EERD, 1 | ((u32)(addr)<< 8));
+	while( !((tmp = read_i8254x(i8254x,REG_EERD)) & (1 << 4)));
+
+	return (uint16_t)((tmp >> 16) & 0xFFFF);
+}
+
+static void read_mac_i8254x(t_i8254x* i8254x)
+{
+	u32 tmp;	
+
+	tmp=read_i8254x(i8254x,0);
+	tmp=(tmp && 0xf);
+	i8254x->low_mac=read_i8254x(i8254x,0);
+
+	tmp=read_i8254x(i8254x,1);
+	tmp=(tmp && 0xf)<<8;
+	i8254x->low_mac | = tmp;
+
+	tmp=read_i8254x(i8254x,2);
+	tmp=(tmp && 0xf)<<16;
+	i8254x->low_mac | = tmp;
+
+	tmp=read_i8254x(i8254x,3);
+	tmp=(tmp && 0xf)<<24;
+	i8254x->low_mac | = tmp;
+
+	tmp=read_i8254x(i8254x,4);
+	tmp=(tmp && 0xf);
+	i8254x->hi_mac=read_i8254x(i8254x,0);
+
+	tmp=read_i8254x(i8254x,5);
+	tmp=(tmp && 0xf)<<8;
+	i8254x->hi_mac | = tmp;
+}
+
+static rx_init_i8254x(t_i8254x* i8254x)
+{
+	int i;
+	char* rx_desc_ring;
+	t_rx_desc_i8254x* rx_desc;
+
+	rx_desc_ring=kmalloc(sizeof(t_rx_desc_i8254x)*NUM_RX_DESC);
+	rx_desc=rx_desc_ring;
+	for (i=0;i<NUM_RX_DESC;i++)
+	{
+		rx_desc[i]->hi_addr=0;
+		rx_desc[i]->low_addr=kmalloc(RX_BUF_SIZE);
+		rx_desc[i]->status=0;
+	}
+	write_i8254x(i8254x,RDBAL_REG,rx_desc_ring);
+	write_i8254x(i8254x,RDBAH_REG,0);
+	write_i8254x(i8254x,RDLEN,NUM_RX_DESC*16);
+	
+	write_i8254x(i8254x,RHD,0);
+	write_i8254x(i8254x,RDT,NUM_RX_DESC-1);
+
+	write_i8254x(i8254x,REG_RCTRL, (RCTL_EN | RCTL_SBP | RCTL_UPE | RCTL_MPE | RCTL_LBM_NONE | RTCL_RDMTS_HALF | RCTL_BAM | RCTL_SECRC  | RCTL_BSIZE_8192));
+}
+
+static tx_init_i8254x(t_i8254x* i8254x)
+{
+	int i;
+	char* tx_desc_ring;
+	t_tx_desc_i8254x* tx_desc;
+
+	tx_desc_ring=kmalloc(sizeof(t_tx_desc_i8254x)*NUM_TX_DESC);
+	tx_desc=tx_desc_ring;
+	for (i=0;i<NUM_TX_DESC;i++)
+	{
+		tx_desc[i]->hi_addr=0;
+		tx_desc[i]->low_addr=kmalloc(RX_BUF_SIZE);
+		tx_desc[i]->status=TSTA_DD;
+		tx_desc[i]->cmd=0;
+	}
+	write_i8254x(i8254x,TDBAL_REG,rx_desc_ring);
+	write_i8254x(i8254x,TDBAH_REG,0);
+	write_i8254x(i8254x,TDLEN,NUM_RX_DESC*16);
+
+	write_i8254x(i8254x,THD,0);
+	write_i8254x(i8254x,TDT,NUM_RX_DESC-1);
+
+	write_i8254x(i8254x,REG_TCTRL,TCTL_EN,
+			    | TCTL_PSP
+        		    | (15 << TCTL_CT_SHIFT)
+        		    | (64 << TCTL_COLD_SHIFT)
+          		    | TCTL_RTLC);
+}
+
+static start_link_i8254x(t_i8254x* i8254x)
+{
+	u32 current_state;
+
+	current_state=read_i8254x(i8254x,CTRL_REG);
+	write_i8254x(i8254x,CTRL_REG,current_state | CTRL_SLU);
+}
+
+static reset_multicast_array(t_i8254x* i8254x)
+{
+	int index;
+
+	for(index = 0; index < 0x80; index++)
+	{
+		write_i8254x(i8254x,MLTC_TBL_ARRY_REG+index*4,0);
+	}
+}
+
 void init_8254x(t_i8254x* i8254x)
 {
 	struct t_i_desc i_desc;
@@ -18,85 +139,28 @@ void init_8254x(t_i8254x* i8254x)
 	tx_init_i8254x(i8254x);
 }
 
-void int_handler_i8254x()
+void init_8254x(t_i8254x* i8254x)
 {
+
+}
+
+void int_handler_i8254x(t_i8254x* i8254x)
+{
+	u32 status;
+
+	status=read_i8254x(i8254x,REG_ICR);
+	if (status & ICR_LSC)
+	{
+		start_link_i8254x(i8254x);
+	}
+	else if (status & ICR_RXT0)
+	{
+
+	}
 
 }
 
 void send_packet_i8254x()
 {
 
-}
-
-static void write_i8254x(t_i8254x* i8254x,u32 address,u32 value)
-{
-	outdw(value,i8254x->mem_base+address);
-}
-
-static u32 read_i8254x(t_i8254x* i8254x,u32 adrress)
-{
-	return indw(i8254x,CTRL_REG);
-}
-
-static read_mac_i8254x(t_i8254x* i8254x)
-{
-
-}
-
-static rx_init_i8254x()
-{
-	int i;
-	char* rx_desc_ring;
-	t_rx_desc_i8254x* rx_desc;
-
-	rx_desc_ring=kmalloc(sizeof(t_rx_desc_i8254x)*NUM_RX_DESC);
-	rx_desc=rx_desc_ring;
-	for (i=0;i<NUM_RX_DESC;i++)
-	{
-		rx_desc[i]->hi_addr=0;
-		rx_desc[i]->low_addr=kmalloc(RX_BUF_SIZE);
-		rx_desc[i]->status=0;
-	}
-	write_i8254x(i8254x,TDBAL_REG,rx_desc_ring);
-	write_i8254x(i8254x,TDBAH_REG,0);
-	write_i8254x(i8254x,TDLEN,NUM_RX_DESC*16);
-	write_i8254x(i8254x,RHD,0);
-	write_i8254x(i8254x,RDT,NUM_RX_DESC-1);
-	write_i8254x(i8254x,REG_RCTRL, (RCTL_EN | RCTL_SBP | RCTL_UPE | RCTL_MPE | RCTL_LBM_NONE | RTCL_RDMTS_HALF | RCTL_BAM | RCTL_SECRC  | RCTL_BSIZE_8192));
-}
-
-static tx_init_i8254x()
-{
-	int i;
-	char* tx_desc_ring;
-	t_tx_desc_i8254x* tx_desc;
-
-	tx_desc_ring=kmalloc(sizeof(t_tx_desc_i8254x)*NUM_TX_DESC);
-	tx_desc=tx_desc_ring;
-	for (i=0;i<NUM_TX_DESC;i++)
-	{
-		tx_desc[i]->hi_addr=0;
-		tx_desc[i]->low_addr=kmalloc(RX_BUF_SIZE);
-		tx_desc[i]->status=TSTA_DD;
-		tx_desc[i]->cmd=0;
-	}
-
-}
-
-static start_link_i8254x(t_i8254x* i8254x)
-{
-	u32 current_state;
-
-	current_state=read_i8254x(i8254x,CTRL_REG);
-	write_i8254x(i8254x,CTRL_REG,current_state | CTRL_SLU);
-}
-
-static reset_multicast_array(t_i8254x* i8254x)
-{
-	int index;
-
-	for(index = 0; index < 0x80; index++)
-	{
-		write_i8254x(i8254x,MLTC_TBL_ARRY_REG+index*4,0);
-	}
 }
--- include/drivers/i8254x/i8254x.h
+++ include/drivers/i8254x/i8254x.h
@@ -16,33 +16,50 @@
 #define CTRL_SLU 		0x40
 
 #define MLTC_TBL_ARRY_REG       0x5200
-#define TDBAL_REG               0x03800 //Transmit Descriptor Base Low
-#define TDBAH_REG               0x03804 //Transmit Descriptor Base High
-#define TDLEN_REG               0x03808 //Transmit Descriptor Length
-#define RHD_REG			0x3810  //Transmit Descriptor Head
-#define RDT_REG			0x3818  //Transmit Descriptor Tail
+#define TDBAL_REG               0x03800 			//Transmit Descriptor Base Low
+#define TDBAH_REG               0x03804 			//Transmit Descriptor Base High
+#define TDLEN_REG               0x03808 			//Transmit Descriptor Length
+#define THD_REG			0x3810  			//Transmit Descriptor Head
+#define TDT_REG			0x3818  			//Transmit Descriptor Tail
 
-#define RDBAL_REG       	0x2800  //Receive Descriptor Base Low
-#define RDBAH_REG  		0x2804  //Receive Descriptor Base High
-#define RDLEN_REG       	0x2808  //Receive Descriptor Length
-#define RHD_REG			0x2810  //Receive Descriptor Head
-#define RDT_REG			0x2818  //Receive Descriptor Tail
+#define REG_TCTRL		0x400				//Trasmit control register
+#define TCTL_EN                 (1 << 1)    			//Transmit Enable
+#define TCTL_PSP         	(1 << 3)    			//Pad Short Packets
+#define TCTL_CT_SHIFT          	0x4           			//Collision Threshold
+#define TCTL_COLD_SHIFT         0xc          			//Collision Distance
+#define TCTL_RTLC               (1 << 24)   			//Re-transmit on Late Collision
 
-#define NUM_RX_DESC             0x20   //Receive payload buffer size
-#define NUM_TX_DESC             0x20   //Transmit payload buffer size
 
-#define RCTRL_REG		0x100 //receive control register
-#define	RCTL_EN 		(1 << 1)    			// Receiver Enable
-#define	RCTL_SBP		(1 << 2)    			// Store Bad Packets
-#define	RCTL_UPE       		(1 << 3)    			// Unicast Promiscuous Enabled
-#define	RCTL_MPE               	(1 << 4)    			// Multicast Promiscuous Enabled
-#define	RCTL_LBM_NONE		(0 << 6)    			// No Loopback
-#define	RTCL_RDMTS_HALF         (0 << 8)    			// Free Buffer Threshold is 1/2 of RDLEN
-#define	RCTL_BAM               	(1 << 15)   			// Broadcast Accept Mode
-#define	RCTL_SECRC             	(1 << 26)   			// Strip Ethernet CRC
+#define RDBAL_REG       	0x2800  			//Receive Descriptor Base Low
+#define RDBAH_REG  		0x2804  			//Receive Descriptor Base High
+#define RDLEN_REG       	0x2808  			//Receive Descriptor Length
+#define RHD_REG			0x2810  			//Receive Descriptor Head
+#define RDT_REG			0x2818  			//Receive Descriptor Tail
+
+#define RCTRL_REG		0x100 				//receive control register
+#define	RCTL_EN 		(1 << 1)    			//Receiver Enable
+#define	RCTL_SBP		(1 << 2)    			//Store Bad Packets
+#define	RCTL_UPE       		(1 << 3)    			//Unicast Promiscuous Enabled
+#define	RCTL_MPE               	(1 << 4)    			//Multicast Promiscuous Enabled
+#define	RCTL_LBM_NONE		(0 << 6)    			//No Loopback
+#define	RTCL_RDMTS_HALF         (0 << 8)    			//Free Buffer Threshold is 1/2 of RDLEN
+#define	RCTL_BAM               	(1 << 15)   			//Broadcast Accept Mode
+#define	RCTL_SECRC             	(1 << 26)   			//Strip Ethernet CRC
 #define	RCTL_BSIZE_8192        	((2 << 16) | (1 << 25))
 
-#define TSTA_DD                 (1 << 0)    			// Descriptor Done
+#define NUM_RX_DESC             0x20   				//Receive payload buffer size
+#define NUM_TX_DESC             0x20   				//Transmit payload buffer size
+
+#define TSTA_DD                 (1 << 0)    			//Descriptor Done
+#define REG_EERD		0x14				//EEPROM Read 
+
+#define REG_ICR			0xC0 				//Interrupt Cause Read 
+#define ICR_LSC			0x4				//Link Status Change
+#define ICR_RXDMT0 		0x10				//Receive Descriptor Minimum Threshold Reached
+#define ICR_RXT0 		0x80				//Receiver Timer Interrupt
+
+
+
 
 typedef struct s_i8254x
 {
