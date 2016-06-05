2d6362f38eb2372d61a860048550f88daeef5e7c
root <root@localhost.localdomain>
2016-06-05 17:48:50 +0200

    commit

---
+++ process/flood.c
@@ -0,0 +1,53 @@
+#include "lib/lib.h"
+#include "udpclient.h"
+
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <netdb.h>
+//#include <sys/types.h> 
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+
+#define PACKET_SIZE 100
+#define BUFSIZE 1024
+#define IP(a,b,c,d)	(d | (c<<8) | (b<<16) | (a<<24))
+
+typedef unsigned int u32;
+typedef unsigned short u16;
+
+int main(int argc, char **argv) 
+{
+	int sockfd,send_len,i,n_to,port;
+    	struct sockaddr_in send_addr;
+	char* pippo="sssssssssssssssssssssssssssssss";
+
+	printf("starting udp_writer... \n");
+	port=20000;
+
+	send_addr.sin_family = AF_INET;
+	((unsigned char*) &(send_addr.sin_addr.s_addr))[0]=172;
+	((unsigned char*) &(send_addr.sin_addr.s_addr))[1]=16;
+	((unsigned char*) &(send_addr.sin_addr.s_addr))[2]=243;
+	((unsigned char*) &(send_addr.sin_addr.s_addr))[3]=1;
+	((unsigned char*) &(send_addr.sin_port))[0]=((unsigned char*) &(port))[1];
+	((unsigned char*) &(send_addr.sin_port))[1]=((unsigned char*) &(port))[0];
+
+	send_len = sizeof(send_addr);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	for (i=0;i<2;i++)
+	{
+		//printf("sending data: %d \n",len);
+  
+		n_to = sendto(sockfd,(pippo),10,0,&send_addr, send_len);
+		//printf("sent data \n");
+	}
+	close(sockfd);
+	printf("ending udp_writer... \n");
+    	exit(0);
+}
---
+++ process/main.c
@@ -0,0 +1,26 @@
+#include "lib/lib.h"  
+
+int main (int argc, char* argv[])
+{	
+	int age;
+	int pid;
+
+	char* reader="/flo";
+	argv=malloc(sizeof(char*)*2);
+	argv[0]=reader;
+	age=0;
+
+	while(1)
+	{
+		pid=fork();
+		if (pid==0)
+		{
+			exec(argv[0],argv);
+		}
+		age++;
+		sleep(30000);
+		check_free_mem();
+	}
+	exit(0);
+}
+
