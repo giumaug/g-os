1fe4159b98ed98cdeebfebfbb0376dbae0a98efa
root <root@localhost.localdomain>
2016-10-11 19:58:33 +0200

    commit

--- data_types/tcp_conn_map.c
+++ data_types/tcp_conn_map.c
@@ -20,20 +20,47 @@ t_tcp_conn_map* tcp_conn_map_init()
 
 void tcp_conn_map_put(t_tcp_conn_map* tcp_conn_map,t_tcp_conn_desc* tcp_conn_desc)
 {
-	t_tcp_conn_map* tmp;
+	t_tcp_conn_desc* cur_conn;
+	t_tcp_conn_desc* new_conn;
 
 	conn_id = tcp_conn_desc->dst_port | (tcp_conn_desc->src_port << 16);
-	tmp = hashtable_get(tcp_conn_map->conn_map,conn_id);
-	if (tmp != NULL)
+	cur_conn = hashtable_get(tcp_conn_map->conn_map,conn_id);
+	if (cur_conn != NULL)
 	{
-		qui!!!!!!!!!!!
+		cur_conn->is_key_unique = 1;
 	}	
-
+	new_conn = tcp_conn_desc_int(tcp_conn_desc->src_port,tcp_conn_desc->dst_port);
+	new_conn->is_key_unique = 1;
+	ll_append(tcp_conn_map->duplicate_conn_list,new_conn);
 }
 
-tcp_conn_map_get
+t_tcp_conn_map* tcp_conn_map_get(t_tcp_conn_map* tcp_conn_map,u16 src_ip,u16 dst_ip,u32 src_ip,u32 dst_ip)
 {
+	u32 conn_id;
+	t_tcp_conn_desc* tcp_conn_desc = NULL;
+	
+	conn_id = dst_port | (src_port << 16);
+	tcp_conn_desc = hashtable_get(tcp_conn_map->conn_map,conn_id);
 
+	if (tcp_conn_desc == NULL)
+	{
+		return NULL;
+	}
+	if (tcp_conn_desc->is_key_unique == 0)
+	{
+		return tcp_conn_desc;
+	}
+	
+	sentinel_node = ll_sentinel(tcp_conn_map->duplicate_conn_list);
+	next=ll_first(tcp_conn_map->duplicate_conn_list);
+	while(next != sentinel_node && !stop)
+	{
+		next_process_context = next->val;
+		quitttttttttttt-------------------------
+	}
+	
+
+	duplicate_conn_list
 }
 
 tcp_conn_map_free
