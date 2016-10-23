ff4127804b08ccf8af3d8b7b4309dc4892def37d
peppe <peppe@localhost.localdomain>
2016-10-23 20:35:13 +0200

    commit

--- data_types/tcp_conn_map.c
+++ data_types/tcp_conn_map.c
@@ -34,42 +34,50 @@ void tcp_conn_map_put(t_tcp_conn_map* tcp_conn_map,t_tcp_conn_desc* tcp_conn_des
 	ll_append(tcp_conn_map->duplicate_conn_list,new_conn);
 }
 
-t_tcp_conn_map* tcp_conn_map_get(t_tcp_conn_map* tcp_conn_map,u16 src_ip,u16 dst_ip,u32 src_ip,u32 dst_ip)
+void tcp_conn_map_remove(t_tcp_conn_map* tcp_conn_map,u16 src_ip,u16 dst_ip,u32 src_ip,u32 dst_ip)
 {
 	u32 conn_id;
 	t_tcp_conn_desc* tcp_conn_desc = NULL;
+	t_tcp_conn_desc* next_tcp_conn_desc = NULL;
+	t_llist_node* sentinel_node = NULL;
+	t_llist_node* next = NULL;
 	
 	conn_id = dst_port | (src_port << 16);
 	tcp_conn_desc = hashtable_get(tcp_conn_map->conn_map,conn_id);
 
 	if (tcp_conn_desc == NULL)
 	{
-		return NULL;
+		return;
 	}
 	if (tcp_conn_desc->is_key_unique == 0)
 	{
-		return tcp_conn_desc;
+		hashtable_remove(tcp_conn_map->conn_map,conn_id);
+		return;
 	}
-	
-	sentinel_node = ll_sentinel(tcp_conn_map->duplicate_conn_list);
-	next=ll_first(tcp_conn_map->duplicate_conn_list);
-	while(next != sentinel_node && !stop)
+	else if (tcp_conn_desc->src_ip == src_ip && tcp_conn_desc->dst_ip == dst_ip && tcp_conn_desc->src_port == src_port && tcp_conn_desc->dst_port == dst_port)
 	{
-		next_process_context = next->val;
-		quitttttttttttt-------------------------
-	}
+		next_tcp_conn_desc = hashtable_remove(tcp_conn_map->duplicate_conn_map,conn_id);
+		while (next_next = hashtable_get(tcp_conn_map->duplicate_conn_map,conn_id) != NULL)
+		{	
 	
-
-	duplicate_conn_list
+		}
+		if (next_next == NULL)
+		{
+			next_tcp_conn_desc->is_unique_key = 0;
+		}
+		hashtable_remove(tcp_conn_map->conn_map,conn_id);
+		hashtable_put(tcp_conn_map->duplicate_conn_map,conn_id,next_tcp_conn_desc);---------qui
+	}
+	else 
+	{
+		
+	}
+	return NULL;
 }
 
-tcp_conn_map_free
+void tcp_conn_map_free(t_tcp_conn_map* tcp_conn_map)
 {
-
+		hashtable_free(tcp_conn_map->conn_map);
+		free_llist(tcp_conn_map->duplicate_conn_list);
+		kfree(t_tcp_conn_map);
 }
-
-
-
-
-
-
