ae17d4cc61c267bac09c5b795ced57f92511a43c
root <root@localhost.localdomain>
2017-01-06 03:54:59 +0100

    commit

--- process/tcpclient.c
+++ process/tcpclient.c
@@ -9,11 +9,34 @@ void main()
 {
 	int sid;
 	char s[100]={},s1[100]={};
-	struct sockaddr_in ssock,csock;
-	sid=socket(AF_INET,SOCK_STREAM,0);
-	ssock.sin_family=AF_INET;
-	ssock.sin_addr.s_addr=inet_addr("192.168.237.124");
-	ssock.sin_port=htons(39734);
+	
+//-----------------LINUX------------------	
+//	struct sockaddr_in ssock,csock;
+//	sid=socket(AF_INET,SOCK_STREAM,0);
+//	ssock.sin_family=AF_INET;
+//	ssock.sin_addr.s_addr=inet_addr("192.168.237.124");
+//	ssock.sin_port=htons(39734);
+//----------------------------------------
+	
+	//G-OS
+	unsigned int ip=IP(172,16,6,1);
+	unsigned int port=21846;
+	int sid;
+    	struct sockaddr_in ssock;
+	
+	send_addr.sin_family = AF_INET;
+	((unsigned char*) &(ssock.sin_addr.s_addr))[0]=172;
+	((unsigned char*) &(ssock.sin_addr.s_addr))[1]=16;
+	((unsigned char*) &(ssock.sin_addr.s_addr))[2]=243;
+	((unsigned char*) &(ssock.sin_addr.s_addr))[3]=1;
+//  	send_addr.sin_port = (unsigned short) port;
+	((unsigned char*) &(ssock.sin_port))[0]=((unsigned char*) &(port))[1];
+	((unsigned char*) &(ssock.sin_port))[1]=((unsigned char*) &(port))[0];
+
+	
+	send_len = sizeof(send_addr);
+	rcv_len = sizeof(rcv_addr);
+	sid = socket(AF_INET, SOCK_DGRAM, 0);
 	
 	connect(sid,(struct sockaddr *)&ssock,sizeof(ssock));
 	printf("\n Enter the string:");
