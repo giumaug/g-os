c38c23e5a35a614c7333718a211079fb5a8b7b50
root <root@localhost.localdomain>
2017-01-08 14:42:06 +0100

    commit

--- start_bridge.sh
+++ start_bridge.sh
@@ -1,9 +1,9 @@
 /usr/sbin/tunctl -t tap0 -u peppe
 brctl addbr virbr0
-brctl addif virbr0 eth1
+brctl addif virbr0 eth2
 brctl addif virbr0 tap0
 ifconfig tap0 up
 ifconfig virbr0 up
-ifconfig virbr0 172.16.243.100 netmask 255.255.255.0
-ifconfig eth1 0.0.0.0 promisc
-route add default gw 172.16.243.2 virbr0
+ifconfig virbr0 192.168.124.100 netmask 255.255.255.0
+ifconfig eth2 0.0.0.0 promisc
+route add default gw 192.168.124.2 virbr0
