1753349a96025f419b41a4698a8b346ddb4eabc9
root <root@localhost.localdomain>
2017-06-04 00:19:36 +0200

    commit

---
+++ amazon_config.sh
@@ -0,0 +1,14 @@
+/usr/sbin/tunctl -t tap0 -u ec2-user
+brctl addbr virbr0
+brctl addif virbr0 eth1
+brctl addif virbr0 tap0
+ifconfig tap0 up
+ifconfig virbr0 up
+ifconfig virbr0 172.16.6.100 netmask 255.255.255.0
+ifconfig eth1 0.0.0.0 promisc
+sleep 3
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A PREROUTING -p tcp --dport 21846 -j DNAT --to-destination 172.16.6.101:21846
+iptables -t nat -A POSTROUTING -p tcp -d 172.16.6.101 --dport 21846 -j SNAT --to-source 172.31.40.158
+cd /home/ec2-user
+losetup -o 1048576 /dev/loop5 ./disk.img
--- start_bridge.sh
+++ start_bridge.sh
@@ -1,9 +1,9 @@
 /usr/sbin/tunctl -t tap0 -u peppe
 brctl addbr virbr0
-brctl addif virbr0 eth2
+brctl addif virbr0 eth3
 brctl addif virbr0 tap0
 ifconfig tap0 up
 ifconfig virbr0 up
 ifconfig virbr0 192.168.124.100 netmask 255.255.255.0
-ifconfig eth2 0.0.0.0 promisc
+ifconfig eth3 0.0.0.0 promisc
 route add default gw 192.168.124.2 virbr0
